97cf6043725882b44ff2d5634e382fb0c5e03d12
Petyo Kostakov <petyo.kostakov@sap.com>
2015-03-12 11:04:42 +0200

    Initial Python Decoration and git ignore

---
+++ python/Decoration/FunctionDecoration.py
@@ -0,0 +1,34 @@
+__author__ = 'I312934'
+
+print("-----------Normal function")
+def string_len(s):
+    return len(s)
+
+print(string_len("some string with some length"))
+print(string_len([1, 2, 3, 4]))
+
+
+def string_required(f):
+    def string_check(args):
+        if type(args) != str:
+            raise TypeError("Passed argument is not a string")
+        return f(args)
+    return string_check
+
+
+print("-----------Decoration without annotation")
+string_len = string_required(string_len)
+
+print(string_len("some string with some length"))
+print(string_len([1, 2, 3, 4]))
+
+
+''' remove this comment and comment the section above in order to test this example
+print("-----------Decoration with annotation")
+@string_required
+def string_len1(s):
+    return len(s)
+
+print(string_len1("some string with some length"))
+print(string_len1([1, 2, 3, 4]))
+'''
