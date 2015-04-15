156b8cbff0c072e3c6695d971d72c1b92b26aae7
Petyo Kostakov <petyo.kostakov@sap.com>
2015-04-15 19:45:22 +0300

    Render page on navigation issue click

--- botron-page/app/js/views/SplitAppPageView.js
+++ botron-page/app/js/views/SplitAppPageView.js
@@ -2,15 +2,40 @@ define(["backbone", "text!/templates/SplitAppPageTemplate.html"],
 function (Backbone, pageTemplate) {
     return Backbone.View.extend({
         template: _.template(pageTemplate),
-        events: {
+       /* events: {
             "click .next-item": "nextItem",
             "click .prev-item": "prevItem"
-        },
+        },*/
         initialize: function () {
             this.render();
         },
         render: function () {
-            this.el.innerHTML = this.template(this.model.toJSON());
+            var collection = this.model.collection,
+                totalRecords = collection.state.totalRecords,
+                modelIndex = collection.indexOf(this.model),
+                currentPage = collection.state.currentPage;
+
+            //this.model.set("position", modelIndex + 1);
+            //this.model.set("totalRecords", collection.state.totalRecords);
+            
+            var obj = {
+                position: ((currentPage - 1) * 10) + modelIndex + 1,
+                totalRecords: collection.state.totalRecords
+            };
+            //this.collection.state.currentPosition(obj.position);
+
+            this.el.innerHTML = this.template(_.extendOwn(obj, this.model.attributes));
         }
+        /*nextItem: function () {
+            var collection = this.model.collection,
+                currentPage = collection.state.currentPage,
+                nextModel = collection.at(currentPage);
+
+            collection.state.currentPage++;
+            this.parent.renderPageView(nextModel);
+        },
+        prevItem: function () {
+            debugger;
+        }*/
     });
 });
--- botron-page/app/js/views/SplitAppView.js
+++ botron-page/app/js/views/SplitAppView.js
@@ -3,25 +3,41 @@ function (Backbone, Issues, IssueView, Page, splitAppTmpt, Pagination, temp) {
   var SplitAppView = Backbone.View.extend({
     template: _.template(splitAppTmpt),
     events: {
-      "click .bt-split-cont-nav-sort": "toggleSort"
+      "click .bt-split-cont-nav-sort": "toggleSort",
+      "click li.bt-list-el": "clickListItem"
     },
+    clickListItem: function (e) {
+      if (this.curruntSelected) {
+        $(this.curruntSelected).removeClass('selected');
+      }
+      var model = this.collection.get(e.currentTarget.id);
+      
+      this.curruntSelected = e.currentTarget;
+
+      $(e.currentTarget ).addClass("selected");
+      this.renderPageView(model);
+    } ,
     initialize: function () {
       this.listenTo(this.collection, 'change', this.rerender);//TODO do not work for some reason
       this.listenTo(this.collection, 'sync', this.rerender);
       
       this.render();
     },
-    childViews: {},
+    childViews: {
+      paginationView: {},
+      pageView: {}
+    },
     render: function () {
-      //TODO refactor use template for jenerating all these
-      var template = this.$el.html(this.template()),
-        $navContUlEl = this.$('.bt-split-cont-nav > .bt-split-cont-nav-ul'),
-        $paginationCont = this.$('.bt-split-cont-nav-pagination'),
-        $pageCont = this.$('.bt-split-cont-page');
+      //TODO refactor use template for generating all these
+      this.$el.html(this.template());
 
-      this.addIssues($navContUlEl);
+      var $navContUlEl = this.$('.bt-split-cont-nav > .bt-split-cont-nav-ul'),
+          $paginationCont = this.$('.bt-split-cont-nav-pagination');
+
+      this.renderIssues($navContUlEl);
       this.childViews.paginationView = new Pagination({el: $paginationCont, collection: this.collection, parent: this});
-      this.childViews.pageView = new Page({el: $pageCont, model: this.collection.models[0], parent: this})
+      this.childViews.pageView = new Page({el: this.$('.bt-split-cont-page'), model: this.collection.models[0]});
+      this.childViews.pageView.parent = this;
 
       return this;
     },
@@ -31,14 +47,9 @@ function (Backbone, Issues, IssueView, Page, splitAppTmpt, Pagination, temp) {
 
       return this;
     },
-    addIssues: function ($el) {
-      this.collection.forEach(function (issue) {
-        issue.view = new IssueView({model: issue, el: $el});
-        $el.append(issue.view.render().el);
-      });
-    },
-    addIssue: function () {
-
+    //todo comment
+    _getModelListNumber: function (model) {
+        model.current = this.collection.indexOf(model);
     },
     toggleSort: function () {
       var collection = this.collection;
@@ -46,6 +57,21 @@ function (Backbone, Issues, IssueView, Page, splitAppTmpt, Pagination, temp) {
       collection.sortDirection = collection.sortDirection * -1;
       collection.sort();
       this.rerender();
+    },
+    // RENDERING CHILDS
+    renderPageView: function (model) {
+      model.view.$el.addClass("selected");
+      this.childViews.pageView.model = model;
+      this.childViews.pageView.render();
+    },
+    renderIssues: function ($el) {
+      var that = this; //TODO get rid of this
+      
+      this.collection.forEach(function (issue) {
+        issue.view = new IssueView({model: issue, el: $el, parent: that});
+        issue.view.parent = that;
+        $el.append(issue.view.render().el);
+      });
     }
   });
   
